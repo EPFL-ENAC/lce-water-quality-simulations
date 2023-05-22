2afb63e4f0f92fb8b3c023121eeeadddcc8d8f52
ymarcon <yannick.marcon@epfl.ch>
2023-05-22 12:39:35 +0200

    fix: Fix wind angle calculation in geojson

---
+++ data/wind.py
@@ -0,0 +1,38 @@
+import json
+import math
+import sys
+
+f = open( sys.argv[1] , 'rb' )
+data = json.load(f)
+f.close()
+
+features = []
+for index, velu in enumerate(data['Velocity_U']):
+    velv = data['Velocity_U'][index]
+    angle = math.atan2(velv, velu) * 180 / math.pi
+    speed = math.sqrt(math.pow(velu, 2) + math.pow(velv, 2))
+    numda = data['numda_Flow_vector'][index]
+    phi = data['phi_Flow_vector'][index]
+    #print(index, velu, velv, angle, speed,numda, phi)
+    feature = {
+        'type': 'Feature',
+        'properties': {
+            'type': 'speed',
+            'Velocity_U': velu,
+            'Velocity_V': velv,
+            'angle': angle,
+            'speed': speed
+        },
+        'geometry': {
+            'type': 'Point',
+            'coordinates': [ numda, phi ]
+        }
+    }
+    features.append(feature)
+
+geo = {
+    'type': 'FeatureCollection',
+    'features': features
+    }
+
+print(json.dumps(geo, sort_keys=True, indent=4))
